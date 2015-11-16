181f4eb26f73f84e44430f61609fcac0221fc47b
Philip Parker <pparker@noverant.com>
2015-11-16 13:19:11 -0500

    Created database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'test_user_1@test.com', username: 'test_user_1', password: 'test')
+User.create(email: 'test_user_2@test.com', username: 'test_user_2', password: 'test')
+
+Business.create(name: 'test_business_1')
+Business.create(name: 'test_business_2')
+
+Review.create(user: User.find_by(username: 'test_user_1'), business: Business.find_by(name: 'test_business_1'), content: 'test review 1', rating: '4')
+Review.create(user: User.find_by(username: 'test_user_1'), business: Business.find_by(name: 'test_business_2'), content: 'test review 2', rating: '3')
+Review.create(user: User.find_by(username: 'test_user_2'), business: Business.find_by(name: 'test_business_2'), content: 'test review 3', rating: '5')
