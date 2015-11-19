60ac4757d830fb5f140e578b564e05c5669cc014
Philip Parker <pparker@noverant.com>
2015-11-19 09:57:00 -0500

    Testing complete for sessions_controller_spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,36 @@
 class SessionsController < ApplicationController
+  before_action :redirect_logged_in_user, only: [:new, :create]
 
   def new
-
   end
 
   def create
+    user = User.find_by username: session_params[:username]
+
+    if user && user.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = 'You have successfully logged in.'
+      redirect_to root_path
+    else
+      flash[:error] = 'Invalid username or password.'
+      redirect_to login_path
+    end
 
   end
 
   def destroy
+    flash[:notice] = 'You have successfully logged out.' if session[:user_id]
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 
+  private
+
+  def redirect_logged_in_user
+    redirect_to root_path if current_user
+  end
+
+  def session_params
+    params.require(:session).permit(:username, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
-  # validates :password, presence: true, on: :create
 end
